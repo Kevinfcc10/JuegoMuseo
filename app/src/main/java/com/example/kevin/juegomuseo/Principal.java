51d7e8743cda627ef917487c3d920df72a9172fb
Kevinfcc10 <kevin_01fcc@hotmail.com>
2018-01-14 16:57:16 -0500

    Creación de la interfaz principal

--- app/src/main/java/com/example/kevin/juegomuseo/Principal.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Principal.java
@@ -1,7 +1,9 @@
 package com.example.kevin.juegomuseo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Principal extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+    }
+
+    public void goLogin(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
