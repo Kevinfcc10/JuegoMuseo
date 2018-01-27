ae213a76835ace92c106676aac2eb25792f66fd4
Kevinfcc10 <kevin_01fcc@hotmail.com>
2018-01-27 16:14:05 -0500

    login con fb funcionamiento correcto

--- app/src/main/java/com/example/kevin/juegomuseo/Login.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Login.java
@@ -3,8 +3,9 @@ package com.example.kevin.juegomuseo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -14,46 +15,49 @@ import com.facebook.login.widget.LoginButton;
 
 public class Login extends AppCompatActivity {
 
-    private TextView info;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-
         setContentView(R.layout.activity_login);
-        info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
-
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                info.setText(
-                        "User ID: "
-                                + loginResult.getAccessToken().getUserId()
-                                + "\n" +
-                                "Auth Token: "
-                                + loginResult.getAccessToken().getToken()
-                );
+
+                Toast.makeText(Login.this,"Correcto", Toast.LENGTH_SHORT).show();
+                goRegistro();
             }
 
             @Override
             public void onCancel() {
-                info.setText("Login attempt canceled.");
+                Toast.makeText(Login.this,"Login attempt canceled.", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onError(FacebookException e) {
-                info.setText("Login attempt failed.");
+                Toast.makeText(Login.this,"Login attempt failed.", Toast.LENGTH_SHORT).show();
             }
         });
+
+        if (AccessToken.getCurrentAccessToken() != null){
+            goRegistro();
+        }
+
     }
+
+    private void goRegistro(){
+        Intent intent = new Intent(this, Registro.class);
+        startActivity(intent);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
