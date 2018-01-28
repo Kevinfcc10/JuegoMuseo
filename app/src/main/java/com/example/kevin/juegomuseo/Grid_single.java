363c534f3b549942c0898e408d159851ba484e06
Kevinfcc10 <kevin_01fcc@hotmail.com>
2018-01-28 14:34:32 -0500

    funcion de prueba

--- app/src/main/java/com/example/kevin/juegomuseo/Grid_single.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Grid_single.java
@@ -10,4 +10,9 @@ public class Grid_single extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_single);
     }
+
+    public void mensaje(){
+
+    }
+
 }
