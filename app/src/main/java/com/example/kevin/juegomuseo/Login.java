c01bcd79364bb00f5114fc536011d6cc85487cc7
CaroCald <carolina.2594@hotmail.com>
2018-01-28 13:23:22 -0500

    Update3
    Update Informacion y Score

--- app/src/main/java/com/example/kevin/juegomuseo/Login.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Login.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class Informacion extends AppCompatActivity {
+
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_informacion);
+        setContentView(R.layout.activity_login);
     }
 
-    public void goScore(View view) {
-        Intent intent = new Intent(this, Score.class);
+    public void goInfo(View view) {
+        Intent intent = new Intent(this, Informacion.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/kevin/juegomuseo/Score.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Score.java
@@ -1,13 +1,17 @@
 package com.example.kevin.juegomuseo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class Niveles extends AppCompatActivity {
+public class Score extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_niveles);
+        setContentView(R.layout.activity_score);
     }
+
+
 }
