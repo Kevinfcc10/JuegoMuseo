78c63495b34c9dddd58432e2f3fc5d81292cb2f8
CaroCald <carolina.2594@hotmail.com>
2018-01-28 13:24:28 -0500

    Revert "Update3"
    This reverts commit c01bcd79364bb00f5114fc536011d6cc85487cc7.

--- app/src/main/java/com/example/kevin/juegomuseo/Informacion.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Informacion.java
@@ -1,20 +1,13 @@
 package com.example.kevin.juegomuseo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Informacion extends AppCompatActivity {
+public class FichaAnimal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_informacion);
-    }
-
-    public void goScore(View view) {
-        Intent intent = new Intent(this, Score.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_ficha_animal);
     }
 }
--- app/src/main/java/com/example/kevin/juegomuseo/Score.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Score.java
@@ -1,17 +1,13 @@
 package com.example.kevin.juegomuseo;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class Score extends AppCompatActivity {
+public class Niveles extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_score);
+        setContentView(R.layout.activity_niveles);
     }
-
-
 }
