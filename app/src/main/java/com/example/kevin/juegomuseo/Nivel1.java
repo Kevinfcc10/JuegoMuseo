840c1a3b6c887cd2b4fb92de7937572c849dbbfa
jeffersoinpm <jefferson.paredes@finantech.com.ec>
2018-02-12 18:04:37 -0500

    Añado funcionalidad de arrastrar imagenes

--- app/src/main/java/com/example/kevin/juegomuseo/Nivel1.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Nivel1.java
@@ -11,31 +11,40 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class Nivel1 extends AppCompatActivity {
-    private ImageView paper;
-    private ImageView trash;
+    private ImageView tucan;
+    private ImageView sahino;
+    private ImageView mataje;
+    private Integer score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setFullScreen();
         setContentView(R.layout.activity_nivel1);
-        paper = (ImageView) findViewById(R.id.paper);
-        paper.setTag("paper");
-        trash = (ImageView) findViewById(R.id.trash);
-        trash.setTag("trash");
+        tucan = (ImageView) findViewById(R.id.paper);
+        tucan.setTag("tucan");
+        sahino = (ImageView) findViewById(R.id.paper2);
+        tucan.setTag("sahino");
+        mataje = (ImageView) findViewById(R.id.trash);
+        mataje.setTag("mataje");
 
-        trash.setOnDragListener(new TrashDragListener(
-                R.drawable.estrellas,
-                R.drawable.chimborazo));
+        mataje.setOnDragListener(new TrashDragListener(
+                R.drawable.mataje2,
+                R.drawable.mataje2));
 
-        trash.setOnClickListener(new View.OnClickListener() {
+        tucan.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                paper.setVisibility(View.VISIBLE);
+            public boolean onLongClick(View view) {
+
+                ClipData data = ClipData.newPlainText("", "");
+                View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                view.setVisibility(View.INVISIBLE);
+                return true;
             }
         });
 
-        paper.setOnLongClickListener(new View.OnLongClickListener() {
+        sahino.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
 
@@ -55,7 +64,7 @@ public class Nivel1 extends AppCompatActivity {
 
 
 
-    private static class TrashDragListener implements View.OnDragListener {
+    private class TrashDragListener implements View.OnDragListener {
 
         private static final String TAG = "TrashDragListener";
 
@@ -87,17 +96,43 @@ public class Nivel1 extends AppCompatActivity {
                     return true;
                 case DragEvent.ACTION_DROP:
                     Log.d(TAG, "onDrag: ACTION_DROP");
-                    hit = true;
+                    switch (draggedView.getId()){
+                        case R.id.paper:
+                            if(containerView.getTag().equals("sahino_mataje")){
+                                containerView.setImageResource(R.drawable.tucan_sahino_mataje);
+                                Log.d(TAG, "onDrag: ACTION_DRAG_EXITED1");
+                            }else{
+                                containerView.setImageResource(R.drawable.tucan_mataje);
+                                containerView.setTag("tucan_mataje");
+                                Log.d(TAG, "onDrag: ACTION_DRAG_EXITED2");
+                            }
+                            hit = true;
+                            break;
+                        case R.id.paper2:
+                            if(containerView.getTag().equals("tucan_mataje")){
+                                containerView.setImageResource(R.drawable.tucan_sahino_mataje);
+                                Log.d(TAG, "onDrag: ACTION_DRAG_EXITED3");
+                            }else{
+                                containerView.setImageResource(R.drawable.sahino_mataje);
+                                containerView.setTag("sahino_mataje");
+                                Log.d(TAG, "onDrag: ACTION_DRAG_EXITED4");
+                            }
+                            hit = true;
+                            break;
+                        default:
+                            break;
+                    }
                     draggedView.post(new Runnable() {
                         @Override
                         public void run() {
                             draggedView.setVisibility(View.GONE);
                         }
                     });
+                    score+=50;
                     return true;
                 case DragEvent.ACTION_DRAG_ENDED:
                     Log.d(TAG, "onDrag: ACTION_DRAG_ENDED");
-                    containerView.setImageResource(normalShape);
+
                     v.setVisibility(View.VISIBLE);
                     if (!hit) {
                         draggedView.post(new Runnable() {
