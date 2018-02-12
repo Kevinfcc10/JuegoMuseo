878eeaca972c20221d97416d60f1d78ff4734abf
Dannypau <dany.ramos456@gmail.com>
2018-02-12 15:28:27 -0500

    añadiendo gift como jugar

--- app/src/main/java/com/example/kevin/juegomuseo/Informacion.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Informacion.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class Informacion extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class Informacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Glide.with(Informacion.this)
+                .load(R.drawable.comojugar)
+                .asGif()
+                .placeholder(R.drawable.comojugar)
+                .crossFade()
+                .into(imageView);
+
     }
 
     public void goNivel(View view){
