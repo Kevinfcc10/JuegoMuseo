a5a02f74ec8e58257161ea5b7deac2a83483f58b
Dannypau <dany.ramos456@gmail.com>
2018-02-12 18:34:31 -0500

    subiendo funcionalidad score

--- app/src/main/java/com/example/kevin/juegomuseo/Score.java
+++ app/src/main/java/com/example/kevin/juegomuseo/Score.java
@@ -1,13 +1,41 @@
 package com.example.kevin.juegomuseo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Score extends AppCompatActivity {
+    private ImageView estrellas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(EXTRA_MESSAGE);
+        estrellas = (ImageView) findViewById(R.id.imageView13);
+       Integer score = Integer.valueOf(message.split(",")[1]);
+       String nivel = message.split(",")[0];
+        TextView nivelText = (TextView) findViewById(R.id.textView13);
+        nivelText.setText(nivel);
+        TextView puntajeText = (TextView) findViewById(R.id.textViewPuntaje);
+        puntajeText.setText(Integer.valueOf(score));
+        if(score>=70){
+            estrellas.setImageResource(R.drawable.estrella1);
+        }else if(score>=35&&score<70){
+            estrellas.setImageResource(R.drawable.estrella2);
+        }else{
+            estrellas.setImageResource(R.drawable.estrella3);
+        }
+
+
     }
+
+
+
 }
