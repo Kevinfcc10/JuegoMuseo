14001998e34999d476f86b3f8f5c00f6e0bdb53b
Kevinfcc10 <kevin_01fcc@hotmail.com>
2018-02-12 20:22:54 -0500

    Interfaz Nivel N, reutilizacion clase grid_single2

---
+++ app/src/main/java/com/example/kevin/juegomuseo/NivelN.java
@@ -0,0 +1,26 @@
+package com.example.kevin.juegomuseo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class NivelN extends AppCompatActivity {
+
+    GridView grid2;
+    int[] imageId = { R.drawable.rana, R.drawable.mataje, R.drawable.tortuga, R.drawable.delfin, R.drawable.sahino, R.drawable.chimborazo};
+    private Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nivel_n);
+
+        CustomGrid2 adapter = new CustomGrid2(NivelN.this, imageId);
+        grid2=(GridView)findViewById(R.id.grid2);
+        grid2.setAdapter(adapter);
+
+
+
+    }
+}
